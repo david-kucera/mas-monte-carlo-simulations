ca2c9af0d17654a4a76fa2f181621ccdd3390d08
David Kučera <kucera.david2002@gmail.com>
2023-12-07 18:48:05 +0100

    Finished Zadanie05_Vestice

--- src/Zadanie04_Namornik.java
+++ src/Zadanie04_Namornik.java
@@ -4,6 +4,46 @@ public class Zadanie04_Namornik {
     public static void spusti() {
         cast1();
         cast2();
+        cast3();
+    }
+
+    private static void cast3() {
+        int pocetRep = 100000;
+        int pocetKrokov = 1000;
+        UniformDiscreteRNG dsc = new UniformDiscreteRNG(0, 5);
+        int celkovo = 0;
+        for (int i = 0; i < pocetRep; i++) {
+            int[] startBod = {0, 0, 0};
+            for (int j = 0; j < pocetKrokov; j++) {
+                int smer = dsc.sample(); // 0 = x+, 1 = x-, 2 = y+, 3 = y-, 4 = z+, 5 = z-
+                switch (smer) {
+                    case 0:
+                        startBod[0] += 1;
+                        break;
+                    case 1:
+                        startBod[0] -= 1;
+                        break;
+                    case 2:
+                        startBod[1] += 1;
+                        break;
+                    case 3:
+                        startBod[1] -= 1;
+                        break;
+                    case 4:
+                        startBod[2] += 1;
+                        break;
+                    case 5:
+                        startBod[2] -= 1;
+                        break;
+                }
+            }
+            celkovo += Math.abs(startBod[0]);
+            celkovo += Math.abs(startBod[1]);
+            celkovo += Math.abs(startBod[2]);
+        }
+        System.out.println("Vysledok tretej casti prikladu: " + celkovo/pocetRep);
+        //double teor = Math.sqrt((6 * pocetKrokov)/Math.PI);
+        //System.out.println("Teoreticky odhad vysledku: " + Math.round(teor));
     }
 
     private static void cast2() {
@@ -32,6 +72,8 @@ public class Zadanie04_Namornik {
             celkovo += Math.abs(startBod[1]);
         }
         System.out.println("Vysledok druhej casti prikladu: " + celkovo/pocetRep);
+        //double teor = Math.sqrt((4 * pocetKrokov)/Math.PI);
+        //System.out.println("Teoreticky odhad vysledku: " + Math.round(teor));
     }
 
     private static void cast1() {
@@ -52,5 +94,7 @@ public class Zadanie04_Namornik {
             celkovo += Math.abs(startBod);
         }
         System.out.println("Vysledok prvej casti prikladu: " + celkovo/pocetRep);
+        //double teor = Math.sqrt((2 * pocetKrokov)/Math.PI);
+        //System.out.println("Teoreticky odhad vysledku: " + Math.round(teor));
     }
 }
