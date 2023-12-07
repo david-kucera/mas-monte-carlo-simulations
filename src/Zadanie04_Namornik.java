a98e6e7efdbf843215f10e2fc1b4eddfac92e385
David Kučera <kucera.david2002@gmail.com>
2023-12-07 18:38:35 +0100

    Druha cast Zadanie05_Vestice

--- src/Zadanie04_Namornik.java
+++ src/Zadanie04_Namornik.java
@@ -2,7 +2,39 @@ import OSPRNG.UniformDiscreteRNG;
 
 public class Zadanie04_Namornik {
     public static void spusti() {
+        cast1();
+        cast2();
+    }
 
+    private static void cast2() {
+        int pocetRep = 100000;
+        int pocetKrokov = 1000;
+        UniformDiscreteRNG dsc = new UniformDiscreteRNG(0, 1);
+        int celkovo = 0;
+        for (int i = 0; i < pocetRep; i++) {
+            int[] startBod = {0, 0};
+            for (int j = 0; j < pocetKrokov; j++) {
+                int smer = dsc.sample(); // 0 = x, 1 = y os
+                int pohyb = dsc.sample(); // 0 = plus, 1 = minus
+                int zmena = 0;
+                if (pohyb == 0) {
+                    zmena = 1;
+                } else {
+                    zmena = -1;
+                }
+                if (smer == 0) {
+                    startBod[0] += zmena;
+                } else {
+                    startBod[1] += zmena;
+                }
+            }
+            celkovo += Math.abs(startBod[0]);
+            celkovo += Math.abs(startBod[1]);
+        }
+        System.out.println("Vysledok druhej casti prikladu: " + celkovo/pocetRep);
+    }
+
+    private static void cast1() {
         int pocetRep = 100000;
         int pocetKrokov = 1000;
         UniformDiscreteRNG dsc = new UniformDiscreteRNG(0, 1);
@@ -19,6 +51,6 @@ public class Zadanie04_Namornik {
             }
             celkovo += Math.abs(startBod);
         }
-        System.out.println(celkovo/pocetRep);
+        System.out.println("Vysledok prvej casti prikladu: " + celkovo/pocetRep);
     }
 }
