9931436fc605bf3dfd195f931020a9a5bd636317
kucera <kucera.david2002@gmail.com>
2023-12-20 16:11:23 +0100

    Fixed Zadanie05_Vestice

--- src/Zadanie05_Vestice.java
+++ src/Zadanie05_Vestice.java
@@ -1,42 +1,79 @@
+import OSPRNG.TriangularRNG;
 import OSPRNG.UniformContinuousRNG;
 
 public class Zadanie05_Vestice {
     public static void spusti() {
+        uniform();
+        triangular();
+    }
+
+    private static void triangular() {
+        System.out.println("Trojuholnikove rozdelenie:");
+        TriangularRNG random = new TriangularRNG(0.3, 0.8, 1.0);
         int pocetReplikacii = 10000000;
         int prvaDruha = 0;
+        int pocPrvaDruha = 0;
         int vsetky = 0;
+        int pocVsetky = 0;
 
         for (int i = 0; i < pocetReplikacii; i++) {
-            boolean otazka = dajOtazku();
-            boolean vestica1 = dajOdpoved();
-            boolean vestica2 = dajOdpoved();
-            boolean vestica3 = dajOdpoved();
+            boolean otazka = true;
+            boolean vestica1 = random.sample() <= 0.8;
+            boolean vestica2 = random.sample() <= 0.8;
+            boolean vestica3 = random.sample() <= 0.8;
 
             if (vestica1 == vestica2) {
+                pocPrvaDruha++;
                 if (otazka == vestica1) {
                     prvaDruha++;
                 }
             }
 
             if (vestica1 == vestica2 && vestica2 == vestica3) {
+                pocVsetky++;
                 if (otazka == vestica1) {
                     vsetky++;
                 }
             }
         }
-        double percPrvaDruha = ((double) prvaDruha / pocetReplikacii) * 100;
-        double percVsetky = ((double) vsetky / pocetReplikacii) * 100;
+        double percPrvaDruha = ((double) prvaDruha / pocPrvaDruha) * 100;
+        double percVsetky = ((double) vsetky / pocVsetky) * 100;
         System.out.println(percPrvaDruha);
         System.out.println(percVsetky);
     }
 
-    private static boolean dajOtazku() {
+    private static void uniform() {
         UniformContinuousRNG random = new UniformContinuousRNG();
-        return random.sample() <= 0.5;
-    }
+        System.out.println("Rovnomerne rozdelenie:");
+        int pocetReplikacii = 10000000;
+        int prvaDruha = 0;
+        int pocPrvaDruha = 0;
+        int vsetky = 0;
+        int pocVsetky = 0;
 
-    private static boolean dajOdpoved() {
-        UniformContinuousRNG random = new UniformContinuousRNG();
-        return random.sample() <= 0.8;
+        for (int i = 0; i < pocetReplikacii; i++) {
+            boolean otazka = true;
+            boolean vestica1 = random.sample() <= 0.8;
+            boolean vestica2 = random.sample() <= 0.8;
+            boolean vestica3 = random.sample() <= 0.8;
+
+            if (vestica1 == vestica2) {
+                pocPrvaDruha++;
+                if (otazka == vestica1) {
+                    prvaDruha++;
+                }
+            }
+
+            if (vestica1 == vestica2 && vestica2 == vestica3) {
+                pocVsetky++;
+                if (otazka == vestica1) {
+                    vsetky++;
+                }
+            }
+        }
+        double percPrvaDruha = ((double) prvaDruha / pocPrvaDruha) * 100;
+        double percVsetky = ((double) vsetky / pocVsetky) * 100;
+        System.out.println(percPrvaDruha);
+        System.out.println(percVsetky);
     }
 }
